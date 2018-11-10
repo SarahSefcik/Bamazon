f97b85021e7a712d207af7a7ec4eeceed2eae77e
Sarah Catherine Sefcik <sefciksa@gmail.com>
2018-11-09 21:44:37 -0500

    finished js file

--- bamazon.sql
+++ bamazon.sql
@@ -13,6 +13,47 @@ CREATE TABLE products (
   PRIMARY KEY (id)
 );
 
+INSERT INTO products (product_name, department_name, price, stock)
+VALUES ("No.2 Pencil", "Office Supplies", 0.25, 500);
 
+INSERT INTO products (product_name, department_name, price, stock)
+VALUES ("Sticky Notes", "Office Supplies", 1.00, 200);
 
+INSERT INTO products (product_name, department_name, price, stock)
+VALUES ("Sharpies", "Office Supplies", 1.25, 350);
 
+INSERT INTO products (product_name, department_name, price, stock)
+VALUES ("Legal Pad", "Office Supplies", 1.00, 150);
+
+INSERT INTO products (product_name, department_name, price, stock)
+VALUES ("5x7 Index Cards", "Office Supplies", 2.00, 75);
+
+INSERT INTO products (product_name, department_name, price, stock)
+VALUES ("Tape", "Office Supplies", 4.00, 125);
+
+INSERT INTO products (product_name, department_name, price, stock)
+VALUES ("Stapler", "Office Supplies", 6.00, 25);
+
+INSERT INTO products (product_name, department_name, price, stock)
+VALUES ("10 Manilla Folders", "Office Supplies", 3.00, 100);
+
+INSERT INTO products (product_name, department_name, price, stock)
+VALUES ("100 Envelopes", "Office Supplies", 8.00, 50);
+
+INSERT INTO products (product_name, department_name, price, stock)
+VALUES ("Copy Paper", "Office Supplies", 4.00, 15);
+
+INSERT INTO products (product_name, department_name, price, stock)
+VALUES ("Couch", "Home & Kitchen", 700.00, 5);
+
+INSERT INTO products (product_name, department_name, price, stock)
+VALUES ("Lamp", "Home & Kitchen", 50.00, 20);
+
+INSERT INTO products (product_name, department_name, price, stock)
+VALUES ("Lamp", "Home & Kitchen", 50.00, 20);
+
+INSERT INTO products (product_name, department_name, price, stock)
+VALUES ("Blender", "Home & Kitchen", 35.00, 30);
+
+INSERT INTO products (product_name, department_name, price, stock)
+VALUES ("Bar Stool", "Home & Kitchen", 90.00, 3);
---
+++ bamazonCustomer.js
@@ -0,0 +1,85 @@
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+var cTable = require("console.table")
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "bamazon_DB"
+});
+
+connection.connect(function (error) {
+  if (error) throw error;
+  console.log("connected as ID " + connection.threadId);
+  console.log("--Bamazon Prime--");
+});
+
+function displayTable() {
+  connection.query("SELECT * FROM products", function (err, results) {
+    if (err) throw err;
+    console.table(results);
+    buy();
+  });
+};
+
+function buy() {
+  connection.query("SELECT * FROM products", function (err, results) {
+    inquirer
+      .prompt([
+        {
+          name: "item",
+          type: "input",
+          message: "What is the id number of the item you would like to purchase?",
+          validate: function (value) {
+            if (isNaN(value) === false) {
+              return true;
+            }
+            return false;
+          }
+        },
+        {
+          name: "amount",
+          type: "input",
+          message: "How many units would you like to purchase at this time?",
+          validate: function (value) {
+            if (isNaN(value) === false) {
+              return true;
+            }
+            return false;
+          }
+        }]).then(function (answer) {
+          for (var i = 0; i < results.length; i++) {
+            if (results[i].id == answer.item) {
+              var productSelected = results[i];
+            }
+          }
+          if (productSelected.stock >= parseInt(answer.amount)) {
+            connection.query(
+              "UPDATE products SET ? WHERE ? ",
+              [
+                {
+                  stock: productSelected.stock - answer.amount
+                },
+                {
+                  id: productSelected.id
+                }
+              ],
+              function (error) {
+                if (error) throw error;
+                console.log("Your purchase has been made for " + (answer.amount) + " of the"(productSelected.product_name) + "(s) at a total of " + (productSelected.price * answer.amount) + ". Thank you for shopping Bamazon Prime!\n");
+                play();
+              });
+          } else {
+            console.log("Unfortunately, we only have " + productSelected.stock + " of the"(productSelected.product_name) + ("(s) at this time. Please purchase less at this time, our shipment is to arrive in 3 days and we will have more ready for you at that time.\n"))
+            play();
+          }
+        })
+  });
+};
+
+function play() {
+  displayTable();
+};
+
+play();
